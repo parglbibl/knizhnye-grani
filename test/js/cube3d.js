022e45346232caef16436db787f41de65022fb19
parglbibl <bibliotekapargolovskaa@gmail.com>
2026-08-03 03:32:27 +0300

    Update cube3d.js

--- test/js/cube3d.js
+++ test/js/cube3d.js
@@ -48,7 +48,7 @@ if (!container) {
         controls.rotateSpeed = 1.0;
         controls.target.set(0, 0, 0);
 
-        // ===== СТОПОР НА ВЕРТИКАЛЬ (только это!) =====
+        // ===== СТОПОР НА ВЕРТИКАЛЬ =====
         controls.minPolarAngle = 0.05;
         controls.maxPolarAngle = Math.PI - 0.05;
         controls.update();
@@ -160,7 +160,6 @@ if (!container) {
         const ambientLight = new THREE.AmbientLight(0x606080, 0.6);
         scene.add(ambientLight);
 
-        // Твой свет в camera.add, НИЧЕГО НЕ МЕНЯЕМ
         const mainLight = new THREE.DirectionalLight(0xffffff, 0.9);
         mainLight.position.set(2, 4, 3);
         camera.add(mainLight);
